1d5c1d179e799898299a1a2ac597cad135211726
Jon Yoli <Jon@Jons-MacBook-Air.local>
2022-03-01 11:34:02 -0500

    second commit

---
+++ PropertyViewWebApp/Server/Models/Amenitites.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PropertyViewWebApp.Server.Models
+{
+    public class Amenitites
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int ListingsId { get; set; }
+        public virtual Listings Listings { get; set; }
+    }
+}
---
+++ PropertyViewWebApp/Server/Models/Listings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PropertyViewWebApp.Server.Models
+{
+    public class Listings
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public int NumberOfBeds { get; set; }
+
+        [Required]
+        public int NumberOfBaths { get; set; }
+
+        [Required]
+        public double Squarefeet { get; set; }
+
+        public int AmenitiesId { get; set; }
+        public virtual Amenitites Amenitites { get; set; }
+
+        public int TypeOfListingId { get; set; }
+        public virtual TypeOfListing TypeOfListing { get; set; }
+
+    }
+}
